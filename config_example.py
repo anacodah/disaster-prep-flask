481ecf608856b6c404a93dd68a64bf6ad57eb6e9
anacodah <anoushkaredekar.work@gmail.com>
2026-02-02 10:12:51 +0530

    Initial commit

---
+++ config_example.py
@@ -0,0 +1,7 @@
+# config_example.py
+class Config:
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "your_password"
+    MYSQL_DB = "your_database"
+    SECRET_KEY = "your_secret_key"
